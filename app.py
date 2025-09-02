9c23ecd62d07f30a79b5a5cdefaaadc5ccf56a97
deerajgr <158696139+deerajgr@users.noreply.github.com>
2025-09-03 00:00:05 +0530

    app.py

--- app.py
+++ app.py
@@ -6,13 +6,8 @@ import requests as re
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import os
-
 # Load variables from .env
 load_dotenv()
-
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
-
 # col1, col2 = st.columns([1, 3])
 
 st.title('Phishing Website Detection using Machine Learning')
@@ -30,12 +25,12 @@ with st.expander("PROJECT DETAILS"):
              'I created my own data set and defined features, some from the literature and some based on manual analysis. '
              'I used requests library to collect data, BeautifulSoup module to parse and extract features. ')
     st.write('The source code and data sets are available in the below Github link:')
-    st.write('_https://github.com/emre-kocyigit/phishing-website-detection-content-based_')
+    st.write('_https://github.com/deerajgr/Detecting-Phishing-Websites-Using-Machine-Learning_')
 
     st.subheader('Data set')
     st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
-    st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
-    st.write('Data set was created in October 2022.')
+    st.write('Totally 1000000 websites ==> **_604000_ legitimate** websites | **_-396000_ phishing** websites')
+    st.write('Data set was created in October 2025.')
 
     # ----- FOR THE PIE CHART ----- #
     labels = 'phishing', 'legitimate'
